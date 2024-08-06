9a2cc4c827ce58392c69b74d5170a83a4116a50a
Subhrajyoti Kundu <142114227+Subhrajyo@users.noreply.github.com>
2024-08-06 10:40:57 +0000

    variables

---
+++ 1st_javascript/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=144443
+let accountEmil="sjk@gmil.com"
+var accountPassword="12345"
+accountCity="kolkata"
+// accountId=2//not allowed
+console.log(accountId);
+accountEmil="hcd@gmail.com"
+accountPassword="43444"
+accountCity="delhi"
+console.table([accountId,accountEmil,accountPassword,accountCity])
